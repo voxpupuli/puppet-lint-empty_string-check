51254221c32bde11452cb8081e8362e59d2520b1
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-01-08 11:40:21 +0100

    Remove obsolete code

--- lib/puppet-lint/plugins/check_empty_string_assignment.rb
+++ lib/puppet-lint/plugins/check_empty_string_assignment.rb
@@ -1,14 +1,4 @@
 PuppetLint.new_check(:empty_string_parameter_assignment) do
-  def empty_string_tokens
-    empty_string_tokens = []
-    tokens.each_with_index do |token, token_idx|
-      empty_string_tokens << token if token.type == :SSTRING && \
-                                      token.prev_code_token.type == :EQUALS && \
-                                      token.value == ''
-    end
-    empty_string_tokens
-  end
-
   def check
     tokens.each_with_index do |token, token_idx|
       if token.type == :SSTRING && token.prev_code_token.type == :EQUALS && \
